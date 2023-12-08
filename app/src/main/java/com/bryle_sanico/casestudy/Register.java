66c174b45662f51501caa0f50da33851c6289b54
sarjsarj <mparminspolight@gmail.com>
2023-12-08 13:59:51 +0800

    ui changes lmk pag may babaguhin

--- app/src/main/java/com/bryle_sanico/casestudy/Register.java
+++ app/src/main/java/com/bryle_sanico/casestudy/Register.java
@@ -3,6 +3,7 @@ package com.bryle_sanico.casestudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
     ImageView docs_placeholder;
+    EditText inputEmail, inputPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,10 @@ public class Register extends AppCompatActivity {
         Button continueBtn = findViewById(R.id.continuebtn);
         RadioButton RbOwner = findViewById(R.id.rbOwner);
         RadioButton RbRenter = findViewById(R.id.rbRenter);
+        Drawable defaultBorder = getDrawable(R.drawable.border);
+        Drawable selectedBorder = getDrawable(R.drawable.selected_border);
+        inputEmail = findViewById(R.id.emailInput);
+        inputPassword = findViewById(R.id.pwInput);
 
         if (RbOwner.isChecked()) {
             docs_placeholder.setVisibility(View.VISIBLE);
@@ -33,6 +39,26 @@ public class Register extends AppCompatActivity {
             public void onClick(View v) {
                     Intent intent = new Intent(Register.this, Confirmation.class);
                     startActivity(intent);
+            }
+        });
+        inputEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    findViewById(R.id.emailInput).setBackground(selectedBorder);
+                } else {
+                    findViewById(R.id.emailInput).setBackground(defaultBorder);
+                }
+            }
+        });
+        inputPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    findViewById(R.id.pwInput).setBackground(selectedBorder);
+                } else {
+                    findViewById(R.id.pwInput).setBackground(defaultBorder);
+                }
             }
         });
     }
